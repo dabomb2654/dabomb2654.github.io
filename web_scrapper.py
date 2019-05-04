42dffb164b5fc36694126cabe85a00444af8da8d
dabomb2654 <hd@u.northwestern.edu>
2019-05-04 14:52:13 -0700

    Add files via upload

--- web_scrapper.py
+++ web_scrapper.py
@@ -14,6 +14,133 @@ urls_to_scrape = [
                   "http://www.takawiki.com/tiki-index.php?page=Performances2002",
                   "http://www.takawiki.com/tiki-index.php?page=Performances2001",
                   "http://www.takawiki.com/tiki-index.php?page=Performances2000",]
+new_urls = [#"http://www.takawiki.com/tiki-index.php?page=The%20way%20Yu%20are%20Dinner%20Show%20(2002)",
+            #"http://www.takawiki.com/tiki-index.php?page=Miracle+of+the+Holy+Star+%28Cosmos+2002-03%29",
+            "http://www.takawiki.com/tiki-index.php?page=S%20Dinner%20Show%20(2002)",
+            "http://www.takawiki.com/tiki-index.php?page=At+the+End+of+A+Long+Spring+%28Moon+2002%29"]
+def run_for_shows(urls):
+    for x in urls:
+        print "URL", x
+        init_dict = {'EnglishTitle':["English Title"],'JapaneseTitle':["Japanese Title"],'RomanizedTitle':["Romanized Title"],'ShowDirector':['Show Director'],'ShowAuthor':['Show Author'],'ShowComposer':['Show Composer'],'ShowChoreographer':['Show Choreographer'],'':[''],}
+        pd_data = pd.DataFrame(data = init_dict, columns = [""])
+        r = requests.get(x)
+        soup = BeautifulSoup(r.text, 'html.parser')
+        #print soup.prettify()
+        all_data = soup.find("div", {"id":"page-data"})
+        #print "ALL", all_data
+        get_title(all_data)
+        #primary_prod = get_prod_staff(all_data)
+        #print primary_prod
+    return 
+def get_title(soup):
+    title_list = [[],[],[]]
+    show_names = [[],[],[]]
+    revue_names = [[],[],[]]
+    titles = ["English Title:", "Japanese Title:", "Romanized Title:"]
+    for child in soup.descendants:
+        for x in range(len(titles)):
+            if child.string == titles[x]:
+                if child.previous_sibling is not None:
+                    if child.previous_sibling.string != child.string:
+                        title_list[x].append(child.string)
+                        cur = child.next_sibling
+                if cur is not None:
+                    if child.previous_sibling is not None:
+                        if child.previous_sibling.string != child.string:
+                            title_list[x].append(cur.string)
+    #check for two shows
+    '''
+    breakdown: 
+    title_list[0] = eng
+    title_list[1] = jpn
+    title_list[2] = rom
+    title_list[0][0] = "English Title:"
+    title_list[0][1] = TITLE IN ENGLISH
+    title_list[1][0] = "Japanese Title:"
+    title_list[1][1] = TITLE IN JAPANESE
+    title_list[2][0] = "Romanized Title:"
+    title_list[2][1] = TITLE IN ROMANIZED
+    '''
+    if "English Title:" in title_list[0]:
+        if "/" in title_list[0][1]:
+            showname_split = title_list[0][1].split("/")
+            print "SPLIT_eng", showname_split
+            show_names[0].append(showname_split[0])
+            revue_names[0].append(showname_split[1])
+    else: 
+        print "No English Split"
+        return
+    if "Japanese Title:" in title_list[1]:
+        if "/" in title_list[1][1]:
+            showname_split = title_list[1][1].split("/")
+            print "SPLIT_jpn", showname_split
+            show_names[1].append(showname_split[0])
+            revue_names[1].append(showname_split[1])
+    else:
+        print "No Japanese Split"
+        return
+    if "Romanized Title:" in title_list[2]:
+        if "/" in title_list[2][1]:
+            showname_split = title_list[2][1].split("/")
+            print "SPLIT_rom", showname_split
+            show_names[2].append(showname_split[0])
+            revue_names[2].append(showname_split[1])
+        else:
+            print "No Rom split"
+    else:
+        #borrow from English
+        if "/" in title_list[0][1]:
+            showname_split = title_list[0][1].split("/")
+            print "SPLIT_eng", showname_split
+            show_names[0].append(showname_split[0])
+            revue_names[0].append(showname_split[1])
+        else:
+            print "No English Split (rom)"
+    print "TITLES", title_list
+    print "SHOWS", show_names
+    print "REVUES", revue_names
+    return title_list, show_names, revue_names
+        
+def get_prod_staff(soup):
+    info_list = []
+    #get director, composer, choreo, and author
+    for child in soup.descendants:
+        to_add = []
+        if child.previous_sibling is not None and child is not None:
+            if child.previous_sibling.string != child.string:
+                cur = child
+                if stop_check_prod(cur) is True:
+                    if cur.string != " ":
+                        to_add.append(cur.string)
+                    cur = cur.next_sibling
+                    if cur.next_sibling == None:
+                        break
+                    while stop_check_prod(cur) == False:
+                        if cur.string != " ":
+                            to_add.append(cur.string)
+                        cur = cur.next_sibling
+        elif child.previous is None and child is not None:
+            info_list.append(child.string)
+        if to_add != []:
+            info_list.append(to_add)
+    return info_list
+def stop_check_prod(string_val):
+    stop = False
+    if string_val == None:
+        return
+    if string_val.string == None:
+        return
+    if "Director" in string_val.string:
+        stop = True
+    elif "Author" in string_val.string:
+        stop = True
+    elif "Choreographer" in string_val.string:
+        stop = True
+    elif "Composer" in string_val.string:
+        stop = True
+    elif "Available" in string_val.string:
+        stop = True
+    return stop
 def run(urls):
     for x in urls_to_scrape:
         year = x[-4:]
@@ -75,5 +202,7 @@ def run(urls):
         path = "C:\Users\Admin\Desktop\performances" + year +".csv"
         pd_data.to_csv(path)
         print "generated file at ", path
-run(urls_to_scrape)
+        
+#run(urls_to_scrape)
+run_for_shows(new_urls)
 
