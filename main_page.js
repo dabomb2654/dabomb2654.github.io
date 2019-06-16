d7c491b3110b7e76149b580bb02b11cca5813d6d
dabomb2654 <hd@u.northwestern.edu>
2019-06-15 21:16:14 -0700

    ajax call

--- main_page.js
+++ main_page.js
@@ -1,8 +1,43 @@
 console.log("HEY! I WORK!");
 $(document).ready(function(){
-	var reader = new FileReader();
-	reader.onload = function (){
-		document.getElementById('out').innerHTML=reader.result;
-	};
-	reader.readAsText('zuka_performances-performances2000.csv','utf8');
+	console.log("GO");
+	$.ajax({
+		url: 'zuka_performances-performances2000.csv',
+		dataType: 'text',
+	}).done(successFunction);
 });
+function successFunction(data) {
+	console.log("HERE");
+  var allRows = data.split(/\r?\n|\r/);
+  var table = '<table>';
+  for (var singleRow = 0; singleRow < allRows.length; singleRow++) {
+    if (singleRow === 0) {
+      table += '<thead>';
+      table += '<tr>';
+    } else {
+      table += '<tr>';
+    }
+    var rowCells = allRows[singleRow].split(',');
+    for (var rowCell = 0; rowCell < rowCells.length; rowCell++) {
+      if (singleRow === 0) {
+        table += '<th>';
+        table += rowCells[rowCell];
+        table += '</th>';
+      } else {
+        table += '<td>';
+        table += rowCells[rowCell];
+        table += '</td>';
+      }
+    }
+    if (singleRow === 0) {
+      table += '</tr>';
+      table += '</thead>';
+      table += '<tbody>';
+    } else {
+      table += '</tr>';
+    }
+  } 
+  table += '</tbody>';
+  table += '</table>';
+  $('body').append(table);
+}
